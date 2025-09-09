f111ba8d836d827f84a8476549706fc6e0757206
Bastien Perez <104252785+bastienperez@users.noreply.github.com>
2025-09-09 11:15:35 +0200

    Update Invoke-OneDriveKnownFoldersLinksFix.ps1

--- Public/OneDrive/Invoke-OneDriveKnownFoldersLinksFix.ps1
+++ Public/OneDrive/Invoke-OneDriveKnownFoldersLinksFix.ps1
@@ -6,7 +6,7 @@ $oneDriveMyDocuments = [Environment]::GetFolderPath('MyDocuments')
 $oneDriveMyPictures = [Environment]::GetFolderPath('MyPictures')
 
 function Invoke-OneDriveKnownFoldersLinksFix {
-	Param(
+	param(
 		[Parameter(Mandatory = $true)]
 		[string]$Folder,
 		[Parameter(Mandatory = $true)]
@@ -20,8 +20,11 @@ function Invoke-OneDriveKnownFoldersLinksFix {
 		
 		if ($null -ne $junctionsFolder) {
 			$junctionsFolder | ForEach-Object {
-				Write-Host "Move junction folder to $_ because it causes issues for the move (access denied on folder if we do not)" -ForegroundColor green
-				$_ | Move-Item -Force c:\
+				$tempFolder = New-Item -Path $env:SystemDrive\temp_OneDrive_$(Get-Date -Format 'yyyyMMdd') -ItemType Directory -ErrorAction SilentlyContinue
+
+				Write-Host "Move junction folder $($_.FullName) to $($tempFolder.FullName) because it causes issues for the move (access denied on folder if we do not)" -ForegroundColor green
+
+				Move-Item -Path $_.FullName -Destination $tempFolder.FullName -Force
 			}
 		}
 
@@ -43,7 +46,7 @@ function Invoke-OneDriveKnownFoldersLinksFix {
 			return
 		}
 
-		Write-Host "Create smybolic link $userProfile\$folder => $oneDriveFolde" -ForegroundColor green
+		Write-Host "Create symbolic link $userProfile\$folder => $oneDriveFolde" -ForegroundColor green
 		cmd /c mklink /J "$userProfile\$Folder" "$oneDriveFolder"
 	}
 }
