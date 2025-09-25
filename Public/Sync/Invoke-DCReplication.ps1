166212bb53164faad252e39ca664f572381f1b22
Bastien Perez <104252785+bastienperez@users.noreply.github.com>
2025-09-25 11:42:17 +0200

    Enhance documentation for Invoke-DCReplication function with detailed descriptions and examples

--- Public/Sync/Invoke-DCReplication.ps1
+++ Public/Sync/Invoke-DCReplication.ps1
@@ -2,11 +2,39 @@
     .SYNOPSIS
     Force Replication on each Domain Controller in the Forest 
     
+    .DESCRIPTION
+    This function initiates replication between Domain Controllers in an Active Directory forest. It can perform either a directed replication between two specified Domain Controllers or a forest-wide replication to synchronize all Domain Controllers.
+    It uses PowerShell remoting to execute the replication commands on the Domain Controllers.
+
+    .PARAMETER SyncAll
+    If specified, the function will perform a forest-wide replication on all Domain Controllers.
+    Default behavior if no parameters are provided.
+
+    .PARAMETER From
+    The source Domain Controller from which to replicate data.
+    If you specify this parameter, you must also specify the `To` parameter.
+
+    .PARAMETER To
+    The target Domain Controller to which data will be replicated.
+    If you specify this parameter, you must also specify the `From` parameter.
+
     .EXAMPLE
     Invoke-DCReplication
+    Initiates forest-wide replication on all Domain Controllers.
+    Same as using the -SyncAll parameter.
   
     .EXAMPLE
     Invoke-DCReplication -Sync All
+    Initiates forest-wide replication on all Domain Controllers.
+    Same as calling the function without parameters.
+
+    .EXAMPLE
+    Invoke-DCReplication -From "DC1" -To "DC2"
+    Initiates directed replication from Domain Controller "DC1" to Domain Controller "DC2".
+
+    .NOTES
+    This function requires the Active Directory PowerShell module.
+    Ensure you have the necessary permissions to perform replication operations.
 
     #>
 
