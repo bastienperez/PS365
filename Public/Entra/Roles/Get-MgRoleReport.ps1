5e589f9ee8cc2e088e3c69fd4d72eb4307a05b2c
Bastien Perez <104252785+bastienperez@users.noreply.github.com>
2025-10-17 09:17:46 +0200

    Add `RecommendationsSync` property

--- Public/Entra/Roles/Get-MgRoleReport.ps1
+++ Public/Entra/Roles/Get-MgRoleReport.ps1
@@ -47,14 +47,18 @@ Written by Bastien Perez (Clidsys.com - ITPro-Tips.com)
 For more Office 365/Microsoft 365 tips and news, check out ITPro-Tips.com.
 
 Version History:
-## [1.8] - 2025-10-08
+## [1.8.1] - 2025-10-17
+### Added
+- Add `RecommendationsSync` property
+
+## [1.8.0] - 2025-10-08
 ### Added
 - Add `IncludeEmptyRoles` switch parameter to get all roles, even the ones without members
 
 ### Changed
 - Use List for mgRoles for better performance
 
-## [1.7] - 2025-04-04
+## [1.7.0] - 2025-04-04
 ### Changed
 - Add scopes for `RoleManagement.Read.All` and `AuditLog.Read.All` permissions
 
@@ -65,7 +69,7 @@ Version History:
 - Add all type objects in the cache array
 - Add `LastNonInteractiveSignInDateTime` property for users
 
-## [1.5] - 2025-02-25
+## [1.5.0] - 2025-02-25
 ### Changed
 - Always return `true` or `false` for `onPremisesSyncEnabled` properties
 - Fix issues with `objectsCacheArray` that was not working
@@ -74,7 +78,7 @@ Version History:
 ### Plannned for next release
 - Switch to `Invoke-MgGraphRequest` instead of `Get-Mg*` CMDlets
 
-## [1.4] - 2025-02-13
+## [1.4.0] - 2025-02-13
 ### Added
 - Sign-in activity tracking for users
 - Account enabled status.
@@ -82,19 +86,19 @@ Version History:
 - Remove old parameters
 - Test if already connected to Microsoft Graph and with the right permissions
 
-## [1.3] - 2024-05-15
+## [1.3.0] - 2024-05-15
 ### Changed
 - Changes not specified.
 
-## [1.2] - 2024-03-13
+## [1.2.0] - 2024-03-13
 ### Changed
 - Changes not specified.
 
-## [1.1] - 2023-12-01
+## [1.1.0] - 2023-12-01
 ### Changed
 - Changes not specified.
 
-## [1.0] - 2023-10-19
+## [1.0.0] - 2023-10-19
 ### Initial Release
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
@@ -122,7 +126,7 @@ function Get-MgRoleReport {
 
     [System.Collections.Generic.List[PSObject]]$rolesMembersArray = @()
     [System.Collections.Generic.List[Object]]$objectsCacheArray = @()
-    [System.Collections.Generic.List[Object]]$mgRolesArray = @()
+    [System.Collections.Generic.List[Object]]$mgRolesArrayAssignment = @()
 
     $modules = @(
         'Microsoft.Graph.Authentication'
@@ -172,13 +176,13 @@ function Get-MgRoleReport {
     Write-Verbose 'Collecting  roles with assignments...'
 
     try {
-        #$mgRolesArray = Get-MgRoleManagementDirectoryRoleDefinition -ErrorAction Stop
+        #$mgRolesArrayAssignment = Get-MgRoleManagementDirectoryRoleDefinition -ErrorAction Stop
         
         Get-MgRoleManagementDirectoryRoleAssignment -All -ExpandProperty Principal | ForEach-Object {
-            $mgRolesArray.Add($_)
+            $mgRolesArrayAssignment.Add($_)
         }
 
-        #$mgRolesArray = (Invoke-MgGraphRequest -Method GET -Uri 'https://graph.microsoft.com/v1.0/roleManagement/directory/roleAssignments' -OutputType PSObject).Value
+        #$mgRolesArrayAssignment = (Invoke-MgGraphRequest -Method GET -Uri 'https://graph.microsoft.com/v1.0/roleManagement/directory/roleAssignments' -OutputType PSObject).Value
 
         $mgRolesDefinition = Get-MgRoleManagementDirectoryRoleAssignment -All -ExpandProperty roleDefinition
 
@@ -188,10 +192,10 @@ function Get-MgRoleReport {
     }
 
     # In *Assignment, we don't have the role definition, so we need to get it and add it to the object
-    foreach ($mgRole in $mgRolesArray) {
+    foreach ($assignment in $mgRolesArrayAssignment) {
         # Add the role definition to the object
-        Add-Member -InputObject $mgRole -MemberType NoteProperty -Name RoleDefinitionExtended -Value ($mgRolesDefinition | Where-Object { $_.id -eq $mgRole.id }).roleDefinition 
-        # Add-Member -InputObject $mgRole -MemberType NoteProperty -Name RoleDefinitionExtended -Value ($mgRolesDefinition | Where-Object { $_.id -eq $mgRole.id }).roleDefinition.description 
+        Add-Member -InputObject $assignment -MemberType NoteProperty -Name RoleDefinitionExtended -Value ($mgRolesDefinition | Where-Object { $_.id -eq $assignment.id }).roleDefinition 
+        # Add-Member -InputObject $assignment -MemberType NoteProperty -Name RoleDefinitionExtended -Value ($mgRolesDefinition | Where-Object { $_.id -eq $assignment.id }).roleDefinition.description 
     } 
     
 
@@ -199,7 +203,7 @@ function Get-MgRoleReport {
         Write-Verbose 'Collecting PIM eligible role assignments...'
         try {
             (Get-MgRoleManagementDirectoryRoleEligibilitySchedule -All -ExpandProperty * -ErrorAction Stop | Select-Object id, principalId, directoryScopeId, roleDefinitionId, status, principal, @{Name = 'RoleDefinitionExtended'; Expression = { $_.roleDefinition } }) | ForEach-Object {
-                $mgRolesArray.Add($_)
+                $mgRolesArrayAssignment.Add($_)
             }
             #$mgRoles += (Invoke-MgGraphRequest -Method GET -Uri 'https://graph.microsoft.com/v1.0/roleManagement/directory/roleEligibilitySchedule' -OutputType PSObject).Value
         }
@@ -208,26 +212,27 @@ function Get-MgRoleReport {
         }
     }
 
-    foreach ($mgRole in $mgRolesArray) {    
-        $principal = switch ($mgRole.principal.AdditionalProperties.'@odata.type') {
-            '#microsoft.graph.user' { $mgRole.principal.AdditionalProperties.userPrincipalName; break }
-            '#microsoft.graph.servicePrincipal' { $mgRole.principal.AdditionalProperties.appId; break }
-            '#microsoft.graph.group' { $mgRole.principalid; break }
+    foreach ($assignment in $mgRolesArrayAssignment) {    
+        $principal = switch ($assignment.principal.AdditionalProperties.'@odata.type') {
+            '#microsoft.graph.user' { $assignment.principal.AdditionalProperties.userPrincipalName; break }
+            '#microsoft.graph.servicePrincipal' { $assignment.principal.AdditionalProperties.appId; break }
+            '#microsoft.graph.group' { $assignment.principalid; break }
             'default' { '-' }
         }
 
         $object = [PSCustomObject][ordered]@{    
             Principal            = $principal
-            PrincipalDisplayName = $mgRole.principal.AdditionalProperties.displayName
-            PrincipalType        = $mgRole.principal.AdditionalProperties.'@odata.type'.Split('.')[-1]
-            PrincipalObjectID    = $mgRole.principal.id
-            AssignedRole         = $mgRole.RoleDefinitionExtended.displayName
-            AssignedRoleScope    = $mgRole.directoryScopeId
-            AssignmentType       = if ($mgRole.status -eq 'Provisioned') { 'Eligible' } else { 'Permanent' }
-            RoleIsBuiltIn        = $mgRole.RoleDefinitionExtended.isBuiltIn
-            RoleTemplate         = $mgRole.RoleDefinitionExtended.templateId
+            PrincipalDisplayName = $assignment.principal.AdditionalProperties.displayName
+            PrincipalType        = $assignment.principal.AdditionalProperties.'@odata.type'.Split('.')[-1]
+            PrincipalObjectID    = $assignment.principal.id
+            AssignedRole         = $assignment.RoleDefinitionExtended.displayName
+            AssignedRoleScope    = $assignment.directoryScopeId
+            AssignmentType       = if ($assignment.status -eq 'Provisioned') { 'Eligible' } else { 'Permanent' }
+            RoleIsBuiltIn        = $assignment.RoleDefinitionExtended.isBuiltIn
+            RoleTemplate         = $assignment.RoleDefinitionExtended.templateId
             DirectMember         = $true
             Recommendations      = 'Check if the user has alternate email or alternate phone number on Microsoft Entra ID'
+            RecommendationsSync  = $null
         }
 
         $rolesMembersArray.Add($object)
@@ -263,13 +268,14 @@ function Get-MgRoleReport {
                     Principal            = $member.AdditionalProperties.userPrincipalName
                     PrincipalDisplayName = $member.AdditionalProperties.displayName
                     PrincipalType        = $memberType
-                    AssignedRole         = $mgRole.RoleDefinitionExtended.displayName
-                    AssignedRoleScope    = $mgRole.directoryScopeId
-                    AssignmentType       = if ($mgRole.status -eq 'Provisioned') { 'Eligible' } else { 'Permanent' }
-                    RoleIsBuiltIn        = $mgRole.RoleDefinitionExtended.isBuiltIn
-                    RoleTemplate         = $mgRole.RoleDefinitionExtended.templateId
+                    AssignedRole         = $assignment.RoleDefinitionExtended.displayName
+                    AssignedRoleScope    = $assignment.directoryScopeId
+                    AssignmentType       = if ($assignment.status -eq 'Provisioned') { 'Eligible' } else { 'Permanent' }
+                    RoleIsBuiltIn        = $assignment.RoleDefinitionExtended.isBuiltIn
+                    RoleTemplate         = $assignment.RoleDefinitionExtended.templateId
                     DirectMember         = $false
                     Recommendations      = 'Check if the user has alternate email or alternate phone number on Microsoft Entra ID'
+                    RecommendationsSync  = $null
                 }
 
                 $rolesMembersArray.Add($object)
@@ -288,6 +294,7 @@ function Get-MgRoleReport {
         RoleTemplate         = 'Not applicable'
         DirectMember         = 'Not applicable'
         Recommendations      = 'Please check this URL to identify if you have partner with admin roles https: / / admin.microsoft.com / AdminPortal / Home#/partners. More information on https://practical365.com/identifying-potential-unwanted-access-by-your-msp-csp-reseller/'
+        RecommendationsSync  = $null
     }    
     
     $rolesMembersArray.Add($object)
@@ -337,7 +344,9 @@ function Get-MgRoleReport {
                     $onPremisesSyncEnabled = [bool]($mgUser.onPremisesSyncEnabled -eq $true)
 
                     $member | Add-Member -MemberType NoteProperty -Name 'OnPremisesSyncEnabled' -Value $onPremisesSyncEnabled
-
+                    if ($onPremisesSyncEnabled) {
+                        $member.RecommendationsSync = 'User is synced from on-premises. Privileged users in Entra ID should be cloud-only accounts.'
+                    }
                     break
                 }
 
@@ -398,10 +407,10 @@ function Get-MgRoleReport {
 
         Write-Verbose 'Collecting all roles...'
         try {
-            #$mgRolesArray = (Invoke-MgGraphRequest -Method GET -Uri 'https://graph.microsoft.com/v1.0/roleManagement/directory/roleDefinitions' -OutputType PSObject).Value
+            #$mgRolesArrayAssignment = (Invoke-MgGraphRequest -Method GET -Uri 'https://graph.microsoft.com/v1.0/roleManagement/directory/roleDefinitions' -OutputType PSObject).Value
             $mgRolesDefinition = Get-MgRoleManagementDirectoryRoleDefinition -All -ErrorAction Stop
 
-            $emptyRoles = $mgRolesDefinition | Where-Object { $mgRolesArray.RoleDefinitionId -notcontains $_.id }
+            $emptyRoles = $mgRolesDefinition | Where-Object { $mgRolesArrayAssignment.RoleDefinitionId -notcontains $_.id }
 
             foreach ($emptyRole in $emptyRoles) {
                 $object = [PSCustomObject][ordered]@{    
