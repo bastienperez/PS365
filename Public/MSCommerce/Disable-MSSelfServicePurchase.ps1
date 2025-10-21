83305000d72001a41c1c5c51583db6953e9dcb8b
Bastien Perez <104252785+bastienperez@users.noreply.github.com>
2025-10-21 20:41:19 +0200

    Add Disable MSSelfServicePurchase

---
+++ Public/MSCommerce/Disable-MSSelfServicePurchase.ps1
@@ -0,0 +1,9 @@
+﻿# Install-Module -Name MSCommerce -Scope CurrentUser
+# Install-PSRessource -Name MSCommerce -Scope CurrentUser
+Import-Module -Name MSCommerce 
+# Global Administrator or Billing Administrator permissions are required to run this script
+Connect-MSCommerce
+Get-MSCommerceProductPolicies -PolicyId AllowSelfServicePurchase | Where-Object { $_.PolicyValue -eq 'Enabled' } | ForEach-Object {
+    Write-Host -ForegroundColor Cyan "Disabling self-service purchase for product: $($_.ProductID)"
+    Update-MSCommerceProductPolicy -PolicyId AllowSelfServicePurchase -ProductId $_.ProductID -Enabled $false  
+}
