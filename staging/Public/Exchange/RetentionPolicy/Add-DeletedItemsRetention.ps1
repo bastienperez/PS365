1ce215a85c8006afc3d69aa28f2159b66281e1e5
Bastien Perez <104252785+bastienperez@users.noreply.github.com>
2025-11-21 14:43:36 +0100

    remove staging folder

--- staging/Public/Exchange/RetentionPolicy/Add-DeletedItemsRetention.ps1
+++
@@ -1,8 +0,0 @@
-function Add-DeletedItemsRetention {
-    New-RetentionPolicyTag -AgeLimitForRetention 30 -RetentionAction DeleteAndAllowRecovery -Name 'Deleted Items Remove After 30 days' -RetentionEnabled $True -MessageClass * -Type DeletedItems
-    [array]$retentionTags = Get-RetentionPolicy -Identity 'Default MRM Policy' | Select-Object -ExpandProperty RetentionPolicyTagLinks
-
-    $RetentionTags += 'Deleted Items Remove After 30 days'
-
-    Set-RetentionPolicy -Identity 'Default MRM Policy' -RetentionPolicyTagLinks $RetentionTags
-}
--- staging/Public/Migration/MailboxMove/BitTitan/Set-DeploymentProSchedule.ps1
+++
@@ -1,9 +0,0 @@
-function Set-DeploymentProSchedule {
-    [CmdletBinding()]
-    param (
-
-    )
-    end {
-        Invoke-SetDeploymentProSchedule | Out-GridView -Title "Results of Set DeploymentPro Schedule"
-    }
-}
