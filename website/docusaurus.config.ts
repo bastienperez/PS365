e2bef87f63b033cb788a5e174a990207103ed3d7
Bastien Perez <104252785+bastienperez@users.noreply.github.com>
2025-12-06 23:48:41 +0100

    Update Docusaurus configuration to disable dark mode and adjust color settings for improved visibility

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -56,7 +56,9 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     colorMode: {
-      respectPrefersColorScheme: true,
+      defaultMode: 'light',
+      disableSwitch: true,
+      respectPrefersColorScheme: false,
     },
     navbar: {
       title: 'PS365',
