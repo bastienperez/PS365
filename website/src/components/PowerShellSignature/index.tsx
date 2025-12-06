c3c3c0f1786cdf6f97252a6aebeb54b8d5e592fc
Bastien Perez <104252785+bastienperez@users.noreply.github.com>
2025-12-06 23:44:04 +0100

    Refactor PowerShellSignature component to use a single signature prop and enhance syntax highlighting with improved CSS styling

--- website/src/components/PowerShellSignature/index.tsx
+++ website/src/components/PowerShellSignature/index.tsx
@@ -1,32 +1,27 @@
 import React from 'react';
 
 interface PowerShellSignatureProps {
-  command: string;
-  parameters?: Array<{
-    name: string;
-    type: string;
-    required?: boolean;
-    description?: string;
-  }>;
+  signature: string;
 }
 
-export default function PowerShellSignature({ command, parameters = [] }: PowerShellSignatureProps) {
+export default function PowerShellSignature({ signature }: PowerShellSignatureProps) {
+  // Parse the PowerShell signature and apply colors
+  const formatSignature = (sig: string) => {
+    return sig
+      // Function names (start of line until first space or bracket)
+      .replace(/^([A-Za-z-]+)/g, '<span class="ps-function">$1</span>')
+      // Parameters (things that start with -)
+      .replace(/(-[A-Za-z]+)/g, '<span class="ps-parameter">$1</span>')
+      // Types (things between < >)
+      .replace(/(<[^>]+>)/g, '<span class="ps-type">$1</span>')
+      // Brackets and syntax characters
+      .replace(/(\[|\]|\|)/g, '<span class="ps-syntax-char">$1</span>');
+  };
+
   return (
-    <div className="command-signature">
-      <div style={{ marginBottom: '0.5rem' }}>
-        <span className="command-name">{command}</span>
-        {parameters.map((param, index) => (
-          <span key={index}>
-            {' '}
-            {!param.required && <span className="optional">[</span>}
-            <span className="parameter">[{param.name}]</span>
-            {' '}
-            <span className="parameter-type">&lt;{param.type}&gt;</span>
-            {!param.required && <span className="optional">]</span>}
-          </span>
-        ))}
-        <span className="optional"> [&lt;CommonParameters&gt;]</span>
-      </div>
-    </div>
+    <div 
+      className="ps-syntax"
+      dangerouslySetInnerHTML={{ __html: formatSignature(signature) }}
+    />
   );
 }
