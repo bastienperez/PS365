4844dd4a75d6ffffbf3643d53653b0dc3222ce40
Bastien Perez <104252785+bastienperez@users.noreply.github.com>
2026-02-12 13:49:09 +0100

    Update Get-EntraIDHybridJoinComputerInfo function to use switch parameter for ExportToExcel and enhance documentation

--- powershell/Public/Entra/Hybrid/Get-EntraIDHybridJoinComputerInfo.ps1
+++ powershell/Public/Entra/Hybrid/Get-EntraIDHybridJoinComputerInfo.ps1
@@ -33,7 +33,7 @@ function Get-EntraIDHybridJoinComputerInfo {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $false)]
-        [string]$ExportToExcel
+        [switch]$ExportToExcel
     )
     
     # Initialize results collection
@@ -43,7 +43,9 @@ function Get-EntraIDHybridJoinComputerInfo {
     $scp = Get-EntraIDHybridJoinSCP
     $results.SCP = $scp
 
-    Write-Host -ForegroundColor Cyan "SCP Details: $scp"
+    if (-not ($ExportToExcel.IsPresent)) {
+        $scp
+    }
 
     # Urls
     $urls = @(
@@ -53,14 +55,18 @@ function Get-EntraIDHybridJoinComputerInfo {
     )
 
     Write-Host -ForegroundColor Cyan 'Testing connectivity to Microsoft Entra ID'
-    $connectivityResults = @()
+    
+    [System.Collections.Generic.List[Object]]$connectivityResults = @()
+    
     foreach ($url in $urls) {
         $result = Test-NetConnection $url -Port 443
-        $connectivityResults += [PSCustomObject][ordered]@{
+        $connectivityResult = [PSCustomObject][ordered]@{
             Url           = $url
             Connected     = $result.TcpTestSucceeded
             RemoteAddress = $result.RemoteAddress
         }
+
+        $connectivityResults.Add($connectivityResult)
         if ($result.TcpTestSucceeded -eq $false) {
             Write-Warning -ForegroundColor Red "Failed to connect to $url"
         }
@@ -68,10 +74,11 @@ function Get-EntraIDHybridJoinComputerInfo {
             Write-Host -ForegroundColor Green "Successfully connected to $url - $($result.RemoteAddress)"
         }
     }
+
     $results.Connectivity = $connectivityResults
 
-    Write-Host -ForegroundColor Cyan "`nTesting Microsoft Entra ID registry keys"
-    $registryInfo = Get-EntraIDHybridJoinComputerRegistryKeys
+    Write-Host -ForegroundColor Cyan 'Testing Microsoft Entra ID registry keys'
+    $registryInfo = Get-EntraIDHybridJoinComputerRegistryKey
     $results.RegistryInfo = $registryInfo
 
     if ($registryInfo.TenantId) {
@@ -84,7 +91,7 @@ function Get-EntraIDHybridJoinComputerInfo {
         Write-Host -ForegroundColor Yellow "Other registry keys: $($registryInfo.OtherKeys)"
     }
 
-    Write-Host -ForegroundColor Cyan "`nGet scheduled tasks for Workplace Join"
+    Write-Host -ForegroundColor Cyan 'Get scheduled tasks for Workplace Join'
     $deviceJoinTask = Get-ScheduledTask -TaskPath '\Microsoft\Windows\Workplace Join\' -TaskName 'Automatic-Device-Join' 
 
     $task = Get-ScheduledTaskInfo -TaskPath ($deviceJoinTask.TaskPath) -TaskName $deviceJoinTask.TaskName
@@ -99,20 +106,20 @@ function Get-EntraIDHybridJoinComputerInfo {
 
     Write-Host -ForegroundColor Cyan "Task: $($task.TaskPath)$($task.TaskName) - Task last result: $($task.LastTaskResult) - LastRunTime: $($task.LastRunTime) - NextRunTime: $($task.NextRunTime)"
 
-    Write-Host -ForegroundColor Cyan "`nGet usercertificate in AD computer object"
+    Write-Host -ForegroundColor Cyan 'Get usercertificate in AD computer object'
 
     $computerName = $env:COMPUTERNAME
 
     $search = [adsisearcher]"(&(ObjectClass=Computer)(cn=$env:computername))"
 
-    $adCertificates = @()
+    [System.Collections.Generic.List[Object]]$adCertificates = @()
     try {
         $computer = $search.FindAll()
     
-        $computer.Properties.usercertificate | ForEach-Object {  
-            $cert = [System.Security.Cryptography.X509Certificates.X509Certificate2]$_ 
+        foreach ($certificate in $computer.Properties.usercertificate) {  
+            $cert = [System.Security.Cryptography.X509Certificates.X509Certificate2]$certificate 
 
-            $adCertificates += [PSCustomObject][ordered]@{ 
+            $adCertificate = [PSCustomObject][ordered]@{ 
                 DnsNameList          = $cert.DnsNameList -join '|' 
                 FriendlyName         = $cert.FriendlyName
                 Subject              = $cert.Subject  
@@ -123,21 +130,32 @@ function Get-EntraIDHybridJoinComputerInfo {
                 ExpiredData          = $cert.NotAfter  
                 SerialNumber         = $cert.SerialNumber  
                 EnhancedKeyUsageList = $cert.EnhancedKeyUsageList -join '|'
-            } 
+            }
+            $adCertificates.Add($adCertificate) 
         }
     }
     catch {
-        Write-Warning "Failed to get $computername  object from AD - $($_.Exception.Message)"
+        Write-Warning "Failed to get $computerName object from AD - $($_.Exception.Message)"
     }
-    $results.ADCertificates = $adCertificates
 
-    Write-Host -ForegroundColor Cyan "`nGet computer certificates in Personal store"
-    $localCertificates = @()
-    Get-ChildItem -Path cert:\LocalMachine\My | ForEach-Object {  
-        $cert = [System.Security.Cryptography.X509Certificates.X509Certificate2]$_ 
+    if (-not ($ExportToExcel.IsPresent)) {
+        $adCertificates
+    }
+    else {
+        $results.ADCertificates = $adCertificates
+    }
 
-        $localCertificates += [PSCustomObject][ordered]@{ 
-            DnsNameList          = $cert.DnsNameList -join '|' 
+    Write-Host -ForegroundColor Cyan 'Get computer certificates in Personal store'
+    
+    [System.Collections.Generic.List[Object]]$localEntraCertificates = @()
+    
+    $entraCertificates = Get-ChildItem -Path cert:\LocalMachine\My | Where-Object { $_.Issuer -like '*MS-Organization-Access*' -or $_.Issuer -like '*MS-Organization-P2P-Access*' }
+    
+    foreach ($certificate in $entraCertificates) {  
+        $cert = [System.Security.Cryptography.X509Certificates.X509Certificate2]$certificate 
+
+        $localCertificate = [PSCustomObject][ordered]@{ 
+            SerialNumber         = $cert.SerialNumber  
             FriendlyName         = $cert.FriendlyName
             Subject              = $cert.Subject  
             Issuer               = $cert.Issuer
@@ -145,38 +163,59 @@ function Get-EntraIDHybridJoinComputerInfo {
             NotBefore            = $cert.NotBefore
             Thumbprint           = $cert.Thumbprint
             ExpiredData          = $cert.NotAfter  
-            SerialNumber         = $cert.SerialNumber  
             EnhancedKeyUsageList = $cert.EnhancedKeyUsageList -join '|'
+            DnsNameList          = $cert.DnsNameList -join '|' 
+
         }
+
+        $localEntraCertificates.Add($localCertificate)
     }
-    $results.LocalCertificates = $localCertificates
 
-    Write-Host -ForegroundColor Cyan "`nGet Hybrid Join details"
-    $events = @()
+    if (-not ($ExportToExcel.IsPresent)) {
+        $localEntraCertificates
+    }
+    else {
+        $results.LocalCertificates = $localEntraCertificates
+    }
+
+    Write-Host -ForegroundColor Cyan 'Get Hybrid Join details'
+    [System.Collections.Generic.List[Object]]$DeviceRegistrationEvent = @()
+
+    $eventObject = [PSCustomObject][ordered]@{
+        TimeCreated = $null
+        Message     = 'No event found'
+    }
+
+    
     try {
-        $event = Get-WinEvent -FilterHashtable @{LogName = 'Microsoft-Windows-User Device Registration/Admin'; Id = 306 }
+        $eventLogs = Get-WinEvent -FilterHashtable @{LogName = 'Microsoft-Windows-User Device Registration/Admin'; Id = 306 } -ErrorAction SilentlyContinue
 
-        if ($event) {
-            $event | ForEach-Object {
-                $events += [PSCustomObject][ordered]@{
-                    TimeCreated = $_.TimeCreated
-                    Message     = $_.Message
+        if ($eventLogs) {
+            foreach ($event in $eventLogs) {
+                $eventObject = [PSCustomObject][ordered]@{
+                    TimeCreated = $event.TimeCreated
+                    Message     = $event.Message
                 }
             }
         }
+
         else {
             Write-Warning 'No event in Microsoft-Windows-User Device Registration/Admin was found'
             # Sometimes, we just need to wait after the logon
             # I have some issue with 360 events and Windows Hello Enterprise provisioning or not domain controller line of sight
-            Write-Warning "You can logoff/login or wait or run`nStart-ScheduledTask -TaskPath '\Microsoft\Windows\Workplace Join\' -TaskName 'Automatic-Device-Join'"
+            Write-Warning "You can logoff/login or wait or runStart-ScheduledTask -TaskPath '\Microsoft\Windows\Workplace Join\' -TaskName 'Automatic-Device-Join'"
         }
+
+        $DeviceRegistrationEvent.Add($eventObject)
     }
     catch {
         Write-Warning "Failed to get events: $($_.Exception.Message)"
+        $DeviceRegistrationEvent.Add($eventObject)
     }
-    $results.Events = $events
+    
+    $results.DeviceRegistrationEvent = $DeviceRegistrationEvent
 
-    Write-Host -ForegroundColor Cyan "`nGet dsregcmd status"
+    Write-Host -ForegroundColor Cyan 'Get dsregcmd status'
 
     # Computer state
     $dsregcmd = (dsregcmd /status | Where-Object { $_ -match ' : ' } | ForEach-Object { $_.Trim() } | ConvertFrom-String -PropertyNames 'Name', 'Value' -Delimiter ' : ')
@@ -184,7 +223,6 @@ function Get-EntraIDHybridJoinComputerInfo {
     $dsregcmdObject = [PSCustomObject][ordered]@{}
 
     foreach ($property in $dsregcmd) {
-        #    $dsregcmdObject.Add($property.Name, $property.Value)
         $dsregcmdObject | Add-Member -NotePropertyName $property.Name -NotePropertyValue $property.Value
     }
 
@@ -259,7 +297,12 @@ function Get-EntraIDHybridJoinComputerInfo {
     $dsregcmdObject
 
     # Export to Excel if requested
-    if ($ExportToExcel) {
+    if ($ExportToExcel.IsPresent) {
+
+        Write-Verbose 'Preparing Excel export...'
+        $now = Get-Date -Format 'yyyy-MM-dd_HHmmss'
+        $excelFilePath = "$($env:userprofile)\$now-EntraIDHybridJoinInfo.xlsx"
+        Write-Verbose "Excel file path: $excelFilePath"
 
         $workbook = @{}
         if ($results.SCP) {
@@ -280,29 +323,28 @@ function Get-EntraIDHybridJoinComputerInfo {
         if ($results.LocalCertificates -and $results.LocalCertificates.Count -gt 0) {
             $workbook.LocalCertificates = $results.LocalCertificates
         }
-        if ($results.Events -and $results.Events.Count -gt 0) {
-            $workbook.Events = $results.Events
+        if ($results.DeviceRegistrationEvent -and $results.DeviceRegistrationEvent.Count -gt 0) {
+            $workbook.DeviceRegistrationEvent = $results.DeviceRegistrationEvent
         }
         if ($results.DsRegCmd) {
             # Convert dsregcmd object to array for Excel export
-            $dsregArray = @()
-            $results.DsRegCmd.PSObject.Properties | ForEach-Object {
-                $dsregArray += [PSCustomObject]@{
-                    Property = $_.Name
-                    Value    = $_.Value
+            [System.Collections.Generic.List[Object]]$dsregArray = @()
+            foreach ($property in $results.DsRegCmd.PSObject.Properties) {
+                $dsregProperty = [PSCustomObject]@{
+                    Property = $property.Name
+                    Value    = $property.Value
                 }
+                $dsregArray.Add($dsregProperty)
             }
             $workbook.DsRegCmd = $dsregArray
         }
 
         # Export each section to a separate worksheet
-        foreach ($sheetName in $workbook.Keys) {
-            $workbook[$sheetName] | Export-Excel -Path $ExportToExcel -WorksheetName $sheetName -AutoSize -TableStyle Medium9
-        }
+        Write-Host -ForegroundColor Cyan "Exporting dynamic groups to Excel file: $excelFilePath"
 
-        Write-Host -ForegroundColor Green "Results exported to: $ExportToExcel"
-    }
-    else {
-        return $results
+        foreach ($sheetName in $workbook.Keys) {
+            Write-Verbose "Exporting $sheetName to Excel..."
+            $workbook[$sheetName] | Export-Excel -Path $excelFilePath -WorksheetName $sheetName -AutoSize -TableStyle Medium9
+        }
     }
 }
